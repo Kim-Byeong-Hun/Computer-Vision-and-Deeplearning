034644667197f7218c5fb45adf21d422dc0980e7
qudgns5129 <byeonghuns98@gmail.com>
2023-06-02 14:10:12 +0900

    chapter9 ongoing
    416 page

---
+++ sources/chapter09/program9_5.py
@@ -0,0 +1,21 @@
+# pixellib라이브러리로 정지영상을 의미불할하기
+import os
+os.chdir('C:/Users/qudgn/Computer-Vision-and-Deeplearning/sources/chapter09')
+
+from pixellib.semantic import semantic_segmentation
+import cv2 as cv
+
+seg = semantic_segmentation()
+seg.load_ade20k_model('deeplabv3_xception65_ade20k.h5')
+
+img_fname = 'busy_street.jpg'
+seg.segmentAsAde20k(img_fname, output_image_name = 'image_new.jpg')
+info1, img_segmented1 = seg.segmentAsAde20k(img_fname)
+info2, img_segmented2 = seg.segmentAsAde20k(img_fname, overlay=True)
+
+cv.imshow('Image original' , cv.imread(img_fname))
+cv.imshow('Image segmention', img_segmented1)
+cv.imshow('Image segmention overlayed', img_segmented2)
+
+cv.waitKey()
+cv.destroyAllWindows()
---
+++ sources/chapter09/program9_7.py
@@ -0,0 +1,14 @@
+# pixellib 라이브러리로 정지영상을 사례분할하기
+from pixellib.instance import instance_segmentation
+import cv2 as cv
+
+seg = instance_segmentation()
+seg.load_model('mask_rcnn_coco.h5')
+
+img_fname = 'busy_street.jpg'
+info, img_segmented = seg.segmentImage(img_fname, show_bboxes=True)
+
+cv.imshow('Image segmention overlayed', img_segmented)
+
+cv.waitKey()
+cv.destroyAllWindows()
